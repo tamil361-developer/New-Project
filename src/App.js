9802343ae7fb0cd3a88564d9a453bbd23d770b45
tamil361-developer <tamileniyan361@gmail.com>
2025-04-02 17:30:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>App</title>
+      </head>
+      <body>
+        <h1>TAMIL</h1>
+        <h1>TAMIL</h1>
+        <h1>TAMIL</h1>
+        <h1>TAMIL</h1>
+        <h1>TAMIL</h1>
+        <h1>TAMIL</h1>
+      </body>
+    </html>
+  );
+}
+
+export default App;
